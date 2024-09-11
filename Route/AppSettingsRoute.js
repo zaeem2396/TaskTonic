d555551fe17160602d5544a6b18920500cea78de
zaeem2396 <zaeemansari87@gmail.com>
2024-09-12 00:33:01 +0530

    imp: removed js files

--- Route/AppSettingsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const appSettings = require('../Controller/AppSettingsController')
-
-router.get('/appSettings', appSettings.getSettings)
-router.post('/appSettings', appSettings.createSettings)
-
-module.exports = router;
