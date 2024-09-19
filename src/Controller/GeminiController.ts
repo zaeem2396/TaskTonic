9036297217f93380af6c92296df9e36f4356b7b1
zaeem2396 <zaeemansari87@gmail.com>
2024-09-20 00:18:37 +0530

    query genie (Gemini)

---
+++ src/Controller/GeminiController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { pool } from '../config/db';
+import Orm from '../Utils/Orm';
+import { RowDataPacket } from 'mysql2/promise';
+
+class GeminiController {
+
+    private genAI: GoogleGenerativeAI;
+    private orm: Orm;
+
+    constructor() {
+        this.genAI = new GoogleGenerativeAI('AIzaSyAECgRfMrnOVU50ct6C-_riQRFZUB-OcqU');
+        this.orm = new Orm()
+    }
+
+    getPrompt = async (req: Request, res: Response) => {
+        try {
+
+            const { message } = req.body
+            
+            const informationSchemaQuery = "SELECT TABLE_NAME, COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = ?"
+            const [informationSchemaQueryResult] = await pool.query<RowDataPacket[]>(informationSchemaQuery, ['todo']);
+            const schemaData = informationSchemaQueryResult.map(row => `${row.TABLE_NAME} (${row.COLUMN_NAME})`).join(', ');
+            
+            const prompt = `This is my database schema: ${schemaData}. ${message}, just give me the SQL query, not the explanation.`;
+
+            const aiModelName = await this.orm.find('appSettings', { name: 'aiModel' })
+            console.log(aiModelName);
+            
+            const model = this.genAI.getGenerativeModel({ model: aiModelName[0].value.toString() });
+
+            const result = await model.generateContent(prompt);
+
+            if (result?.response?.candidates && result.response.candidates.length > 0) {
+                const contentParts = result.response.candidates.map((candidate: any) => candidate.content?.parts || []).flat();
+                return res.status(200).json(contentParts);
+            } else {
+                return res.status(200).json({ message: "No content generated" });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: error });
+        }
+    };
+}
+
+export default GeminiController
--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, Router } from 'express'
 import UserController from '../Controller/UserController'
+import GeminiController from '../Controller/GeminiController'
 
 const router: Router = express.Router()
 const userController = new UserController()
+const geminiController = new GeminiController
 
 router.post('/createUser', (req: Request, res: Response) => {
     userController.create(req, res)
@@ -15,6 +17,10 @@ router.post('/get', (req: Request, res: Response) => {
 })
 router.patch('/update', (req: Request, res: Response) => {
     userController.update(req, res)
+})
+
+router.get('/gemini', (req: Request, res: Response) => {
+    geminiController.getPrompt(req, res)
 })
 
 export default router
