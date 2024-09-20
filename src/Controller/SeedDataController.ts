4f2d71f1cfbf77605fce2de0be7de3f23b340736
zaeem2396 <zaeemansari87@gmail.com>
2024-09-20 22:05:05 +0530

    Populate user table created

--- src/Controller/SeedDataController.ts
+++ src/Controller/SeedDataController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { faker } from '@faker-js/faker'
+import bcrypt from 'bcryptjs'
 import Orm from '../Utils/Orm'
 import Lib from "../Utils/Lib";
 
@@ -22,21 +23,21 @@ class SeedDataController {
 
             for (let i = 0; i < count; i++) {
                 let assignee: string;
-                let assignTo: string;
+                let assignedTo: string;
 
                 do {
                     assignee = this.faker.string.numeric();
                 } while (assignee === "0");
 
                 do {
-                    assignTo = this.faker.string.numeric();
-                } while (assignTo === "0");
+                    assignedTo = this.faker.string.numeric();
+                } while (assignedTo === "0");
 
                 const data = {
                     title: this.faker.lorem.sentence(),
                     description: this.faker.lorem.paragraph(),
-                    assignee: this.faker.string.numeric(),
-                    assignedTo: this.faker.string.numeric(),
+                    assignee: assignee,
+                    assignedTo: assignedTo,
                     priority: this.faker.helpers.arrayElement(['low', 'medium', 'high']),
                     due_date: this.lib.formatDate(this.faker.date.future()),
                     status: this.faker.helpers.arrayElement(['pending', 'in_progress', 'completed']),
@@ -65,6 +66,44 @@ class SeedDataController {
             return res.status(500).json({ message: 'Error while populating DB', error });
         }
     };
+
+    populateUsersTable = async (req: Request, res: Response) => {
+        try {
+            const { count } = req.body
+
+            let successCount = 0
+            let failureCount = 0
+
+            for (let i = 0; i < count; i++) {
+                const data = {
+                    name: this.faker.person.fullName(),
+                    email: this.faker.internet.email(),
+                    password: bcrypt.hashSync('Task@123', 10),
+                    role: 'EMP',
+                    created_at: this.lib.formatDate(this.faker.date.past()),
+                    updated_at: this.lib.formatDate(this.faker.date.recent())
+                }
+
+                const result = await this.orm.create('users', data)
+                if (result.affectedRows === 1) {
+                    successCount++
+                } else {
+                    failureCount++
+                }
+            }
+            if (successCount > 0) {
+                return res.status(200).json({
+                    message: `Successfully populated ${successCount} users`,
+                    failures: failureCount
+                });
+            } else {
+                return res.status(500).json({ message: 'No tasks were populated', failures: failureCount });
+            }
+        } catch (error) {
+            console.log(`Error while populating DB: ${error}`);
+            return res.status(500).json({ message: 'Error while populating DB', error });
+        }
+    }
 }
 
 export default SeedDataController
--- src/Routes/SeedRoute.ts
+++ src/Routes/SeedRoute.ts
@@ -4,8 +4,11 @@ import SeedDataController from '../Controller/SeedDataController';
 const router: Router = express.Router();
 const seedDataController = new SeedDataController();
 
-router.post('/seedData', (req: Request, res: Response) => {
+router.post('/seedTask', (req: Request, res: Response) => {
     seedDataController.populateTaskData(req, res)
+})
+router.post('/seedUsers', (req: Request, res: Response) => {
+    seedDataController.populateUsersTable(req, res)
 })
 
 export default router
